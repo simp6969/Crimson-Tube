5efa7782528a193c78fa3d8ee81c3531b6324fc1
Hu Tao <ariunboldbold200@gmail.com>
2026-01-15 02:13:49 +0800

    vercel upload fix hopefully

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  experimental: {
+    // This tells Next.js to include these files in the serverless bundle
+    outputFileTracingIncludes: {
+      "/api/**/*": ["./node_modules/youtube-dl-exec/bin/**/*"],
+    },
+  },
+};
 
 export default nextConfig;
